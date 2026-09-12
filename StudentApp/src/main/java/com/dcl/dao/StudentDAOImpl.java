0651b179f6258d0a9029d592951d81f3b66917d4
PunithB1601 <punithb072@gmail.com>
2026-09-12 11:33:34 +0530

    jdbc logic added to dao

--- StudentApp/src/main/java/com/dcl/dao/StudentDAOImpl.java
+++ StudentApp/src/main/java/com/dcl/dao/StudentDAOImpl.java
@@ -1,45 +1,143 @@
 package com.dcl.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.dcl.dto.Student;
+import com.dcl.util.ConnectionFactory;
 
 public class StudentDAOImpl implements StudentDAO {
 
+	private Connection con;
+	
+	public StudentDAOImpl() {
+		this.con=ConnectionFactory.getCon();
+	}
+	
 	@Override
 	public void insertStudent(Student s) {
-		// TODO Auto-generated method stub
+		String query="INSERT INTO STUDENT VALUES (0,?,?,?,?)";
+		try {
+			PreparedStatement ps=con.prepareStatement(query);
+			ps.setString(1,s.getName());
+			ps.setString(2, s.getPhone());
+			ps.setString(3, s.getEmail());
+			ps.setString(4, s.getPassword());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public void updateStudent(Student s) {
-		// TODO Auto-generated method stub
+		String query="UPDATE STUDENT SET NAME=?, PHONE=?, EMAIL=?, PASSWORD=? WHERE ID=?";
+		try {
+			PreparedStatement ps=con.prepareStatement(query);
+			ps.setString(1, s.getName());
+			ps.setString(2, s.getPhone());
+			ps.setString(3, s.getEmail());
+			ps.setString(4, s.getPassword());
+			ps.setInt(5, s.getId());
+			ps.executeUpdate();	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
 	@Override
 	public void deleteStudent(Integer id) {
-		// TODO Auto-generated method stub
+		String query="DELETE FROM STUDENT WHERE ID=?";
+		try {
+			PreparedStatement ps=con.prepareStatement(query);
+			ps.setInt(1, id);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
 
 	}
 
 	@Override
 	public Student getStudentById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Student s=null;
+		String query="SELECT * FROM STUDENT WHERE ID=?";
+		try {
+			PreparedStatement ps=con.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				s=new Student();
+				s.setId(rs.getInt("id")); 
+				s.setName(rs.getString("name"));
+				s.setPhone(rs.getString("phone"));
+				s.setEmail(rs.getString("email"));
+				s.setPassword(rs.getString("password"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return s;
 	}
 
 	@Override
 	public Student getStudentByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Student s=null;
+		String query="SELECT * FROM STUDENT WHERE EMAIL=?";
+		try {
+			PreparedStatement ps=con.prepareStatement(query);
+			ps.setString(1, email);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				s=new Student();
+				s.setId(rs.getInt("id")); 
+				s.setName(rs.getString("name"));
+				s.setPhone(rs.getString("phone"));
+				s.setEmail(rs.getString("email"));
+				s.setPassword(rs.getString("password"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return s;
 	}
 
 	@Override
 	public List<Student> getAllStudent() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Student> studentList=new ArrayList();
+		Student s=null;
+		String query="SELECT * FROM STUDENT";
+		try {
+			PreparedStatement ps=con.prepareStatement(query);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				s=new Student();
+				s.setId(rs.getInt("id")); 
+				s.setName(rs.getString("name"));
+				s.setPhone(rs.getString("phone"));
+				s.setEmail(rs.getString("email"));
+				s.setPassword(rs.getString("password"));
+				studentList.add(s);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return studentList;
 	}
 
 }
