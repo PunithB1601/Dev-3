639ff3cae36fab17abfc0a9569483d6f9151e69e
PunithB1601 <punithb072@gmail.com>
2026-09-25 11:07:02 +0530

    update logic added

---
+++ StudentApp/src/main/java/com/dcl/servlet/Update.java
@@ -0,0 +1,40 @@
+package com.dcl.servlet;
+
+import java.io.IOException;
+
+import com.dcl.dao.StudentDAO;
+import com.dcl.dao.StudentDAOImpl;
+import com.dcl.dto.Student;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/update")
+public class Update extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		StudentDAO sdao=new StudentDAOImpl();
+		HttpSession session=req.getSession();
+		Student sessionStudent=(Student)session.getAttribute("student");
+		Student s=sdao.getStudentById(Integer.parseInt(req.getParameter("id")));
+		
+		if(s!=null&&s.getId()==sessionStudent.getId()) {
+			sessionStudent.setName(req.getParameter("name"));
+			sessionStudent.setPhone(req.getParameter("phone"));
+			sessionStudent.setEmail(req.getParameter("email"));
+			sdao.updateStudent(s);
+			req.setAttribute("success-message", "Student Updated successfully!");
+			req.getRequestDispatcher("dashboard.jsp").forward(req, resp);
+		}
+		else {
+			req.setAttribute("error-message", "Invalid Student Info!, Kindly login");
+			req.getRequestDispatcher("login.jsp").forward(req, resp);
+		}
+	}
+}
--- StudentApp/src/main/webapp/updateAccount.jsp
+++ StudentApp/src/main/webapp/updateAccount.jsp
@@ -9,6 +9,7 @@
 </head>
 <body>
 <%Student s=(Student)session.getAttribute("student");%>
+<%if(s!=null){%>
 <h1>Student Update</h1>
 	<form action="update" method="POST">
 	<table>
@@ -24,8 +25,9 @@
 			<td>Enter the Email:</td>
 			<td><input type="email" name="email" value="<%=s.getEmail()%>"></td>
 		</tr>
-		
+			
 		<tr>
+			<input type="hidden" name="id" value="<%=s.getId()%>">
 			<td><button type="submit" class="bg-blue-900 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-full">
   Update Account
 </button></td>
@@ -35,5 +37,9 @@
 		</tr>
 	</table>
 	</form>
+	<%}else{%>
+		<%request.setAttribute("error-message", "Session expired!");%>
+		<%request.getRequestDispatcher("login.jsp").forward(request, response);%>
+	<%}%>
 </body>
 </html>
